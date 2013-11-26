f66a94453f8a5f9ed2f38d92a57e7635030efcb3
Simon Roosen <smnroosen@googlemail.com>
2013-11-26 01:44:11 +0100

    Added Files after reconstructing.

---
+++ src/temp/network/NetworkConnection.java
@@ -0,0 +1,50 @@
+/*
+ * TEMP
+ * Copyright (C) 2012-2013 Simon Roosen, Lukas Glitt, Olaf Matticzk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package temp.network;
+
+import temp.base.ModuleLinker;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class NetworkConnection {
+
+	Socket socket;
+
+	public NetworkConnection() {
+	}
+
+	public void connect(Object[] args) {
+		if (socket != null && !socket.isClosed()) {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				ModuleLinker.reportError("socket can't be closed\n" + socket.toString());
+			}
+		}
+	}
+
+
+	public void disconnect(Object[] args) {
+		try {
+			socket.close();
+		} catch (IOException e) {
+			ModuleLinker.reportError("socket can't be closed\n" + socket.toString());
+		}
+	}
+}
